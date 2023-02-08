239f8cbb33b19d27cb27bf07f3aa05c2d9e86271
howisitgoing <howisitgoing@kakao.com>
2023-02-08 16:33:17 +0900

    Initial commit

---
+++ src/test/java/com/study/purejpa/PureJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.purejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
