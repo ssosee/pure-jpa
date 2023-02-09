a8d2291c6ba24fd087c1e8ad98c0e87f9c786bec
howisitgoing <howisitgoing@kakao.com>
2023-02-09 15:01:58 +0900

    JPA CRUD 맛보기

--- src/main/java/com/study/purejpa/jpa/JpaMain.java
+++ src/main/java/com/study/purejpa/jpa/JpaMain.java
@@ -8,22 +8,82 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
+
     public static void main(String[] args) {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pureJpa");
         EntityManager em = emf.createEntityManager();
 
+//        Member member = new Member();
+//        member.setId(1L);
+//        member.setName("아이유");
+//        saveMember(member, em, emf);
+
+        //Member member = findMember(1L, em, emf);
+        updateMemberName(1L, "밍키밍카이", em, emf);
+    }
+
+    private static void saveMember(Member member, EntityManager em, EntityManagerFactory emf) {
+
         EntityTransaction tx = em.getTransaction(); // 트랜잭션 얻음
         tx.begin(); // 트랜잭션 시작
+        try {
+            em.persist(member);
+            tx.commit(); // 커밋
+        } catch (Exception e) {
+            tx.rollback(); // 롤백
+        } finally {
+            em.close(); // DB Connection을 사용하여 작업함
+        }
+        emf.close(); // 애플리케이션이 종료되면 닫아야함
+    }
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("세웅");
-        em.persist(member);
+    private static Member findMember(Long id, EntityManager em, EntityManagerFactory emf) {
 
-        tx.commit(); // 커밋
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션 얻음
+        Member member = null;
+        tx.begin(); // 트랜잭션 시작
+        try {
+            member = em.find(Member.class, id); // 영속성 컨텍스트로 관리
+            tx.commit(); // 커밋
+        } catch (Exception e) {
+            tx.rollback(); // 롤백
+        } finally {
+            em.close(); // DB Connection을 사용하여 작업함
+        }
+        emf.close(); // 애플리케이션이 종료되면 닫아야함
+        return member;
+    }
 
-        em.close();
+    private static void updateMemberName(Long id, String name, EntityManager em, EntityManagerFactory emf) {
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션 얻음
+        tx.begin(); // 트랜잭션 시작
+        try {
+            Member findMember = em.find(Member.class, id);
+            findMember.setName(name);
+            tx.commit(); // 커밋
+        } catch (Exception e) {
+            tx.rollback(); // 롤백
+        } finally {
+            em.close(); // DB Connection을 사용하여 작업함
+        }
+        emf.close(); // 애플리케이션이 종료되면 닫아야함
+    }
 
+    private static void deleteMember(Long id, EntityManager em, EntityManagerFactory emf) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // 트랜잭션 시작
+
+        try {
+            Member findMember = em.find(Member.class, id);
+            em.remove(findMember); // 삭제
+
+            tx.commit(); // 커밋
+        } catch (Exception e) {
+            tx.rollback(); // 롤백
+        } finally {
+            em.close(); // DB Connection을 사용하여 작업함
+        }
         emf.close(); // 애플리케이션이 종료되면 닫아야함
     }
 }
